a50d1438fc5f5676397cadf2cee41f336adaa057
PatV3-0 <130934721+PatV3-0@users.noreply.github.com>
2025-04-09 18:05:41 +0200

    Update jest.config.js

--- rest-api/jest.config.js
+++ rest-api/jest.config.js
@@ -1,9 +1,9 @@
-export default {
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest', // This tells Jest to transform .js or .jsx files using babel
-    },
-    testEnvironment: 'node',
-    moduleNameMapper: {
-      '^(\\.{1,2}/.*)\\.js$': '$1',  // Handle relative imports
-    },
-  };
+module.exports = {
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest', // This tells Jest to transform .js or .jsx files using babel
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',  // Handle relative imports
+  },
+};
